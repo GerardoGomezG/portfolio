824794bfa011ee5ad490004cf4b116bd2476f288
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-04 18:59:21 -0400

    Adds popup window in my work section

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+// Toogles hamburger button and mobile menu display
 const x = document.querySelector('.menu-button');
 x.addEventListener('click', () => {
   x.classList.toggle('change');
+});
+
+// Popup window
+const openModalBtn1 = document.querySelector('#openModal1');
+const openModalBtn2 = document.querySelector('#openModal2');
+const closeModalBtn = document.querySelector('#closeModal');
+const modal = document.querySelector('#modal');
+
+openModalBtn1.addEventListener('click', () => {
+  modal.showModal();
+});
+
+openModalBtn2.addEventListener('click', () => {
+  modal.showModal();
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.close();
 });
