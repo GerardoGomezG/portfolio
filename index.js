21379c5ad86613edd5d3cab54441a6339ae776f7
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-08 15:47:37 -0400

    Adds hover effect to the 1st element of .newsBlog

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const projectCard = [{
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
+  alt: 'project snapshot',
 },
 {
   id: 'card02',
@@ -22,33 +23,41 @@ const projectCard = [{
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
+  alt: 'project snapshot',
 },
 {
   id: 'card03',
   title: 'News Blog 3',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  src: '(./images/travel.webp)',
+  src: './images/travel.webp',
+  alt: 'project snapshot',
 },
 {
   id: 'card03',
-  title: 'News Blog 3',
+  title: 'News Blog 4',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  src: '(./images/travel.webp)',
+  src: './images/travel.webp',
+  alt: 'project snapshot',
 },
 {
   id: 'card03',
-  title: 'News Blog 3',
+  title: 'News Blog 5',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  src: '(./images/travel.webp)',
+  src: './images/travel.webp',
+  alt: 'project snapshot',
 }];
 
 projectCard.forEach((element) => {
   template.querySelector('.newsBlog h3').textContent = element.title;
   template.querySelector('.newsBlog p').textContent = element.subtitle;
   template.querySelector('.description').textContent = element.description;
+  template.querySelector('#projectImg').setAttribute('src', element.src);
+  template.querySelector('#modal h3').textContent = element.title;
+  template.querySelector('#modal p').textContent = element.subtitle;
+  template.querySelector('#description').textContent = element.description;
 
   const clone = document.importNode(template, true);
   fragment.appendChild(clone);
@@ -57,7 +66,6 @@ projectCard.forEach((element) => {
 works.appendChild(fragment);
 
 // Popup window
-
 const openModalBtn = document.querySelector('#openModal');
 const closeModalBtn = document.querySelector('#closeModal');
 const modal = document.querySelector('#modal');
@@ -68,4 +76,13 @@ openModalBtn.addEventListener('click', () => {
 
 closeModalBtn.addEventListener('click', () => {
   modal.close();
+});
+
+const y = document.querySelector('.newsBlog');
+y.addEventListener('mouseenter', () => {
+  y.classList.toggle('newsBlogHover');
+});
+
+y.addEventListener('mouseleave', () => {
+  y.classList.toggle('newsBlogHover');
 });
