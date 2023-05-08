0c6120e1a27cd0eb6d573fae516b60277423349f
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-08 13:39:19 -0400

    Shows text content created dynamically with JS

--- index.js
+++ index.js
@@ -5,19 +5,74 @@ x.addEventListener('click', () => {
 });
 
 // Popup window
-const openModalBtn1 = document.querySelector('#openModal1');
-const openModalBtn2 = document.querySelector('#openModal2');
-const closeModalBtn = document.querySelector('#closeModal');
-const modal = document.querySelector('#modal');
 
-openModalBtn1.addEventListener('click', () => {
-  modal.showModal();
+// Static design: start
+// const openModalBtn = document.querySelector('#openModal');
+// const openModalBtn2 = document.querySelector('#openModal2');
+// const closeModalBtn = document.querySelector('#closeModal');
+// const modal = document.querySelector('#modal');
+
+// openModalBtn.addEventListener('click', () => {
+// modal.showModal();
+// });
+
+// openModalBtn2.addEventListener('click', () => {
+// modal.showModal();
+// });
+
+// closeModalBtn.addEventListener('click', () => {
+// modal.close();
+// });
+// Static design: end
+
+// Dynamic design: start
+const works = document.querySelector('.myWorks');
+const template = document.querySelector('template').content;
+const fragment = document.createDocumentFragment();
+
+const projectCard = [{
+  id: 'card01',
+  title: 'News Blog 1',
+  subtitle: 'HTML, SCSS, JS',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  src: './images/travel.webp',
+},
+{
+  id: 'card02',
+  title: 'News Blog 2',
+  subtitle: 'HTML, SCSS, JS',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  src: './images/travel.webp',
+},
+{
+  id: 'card03',
+  title: 'News Blog 3',
+  subtitle: 'HTML, SCSS, JS',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  src: '(./images/travel.webp)',
+},
+{
+  id: 'card03',
+  title: 'News Blog 3',
+  subtitle: 'HTML, SCSS, JS',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  src: '(./images/travel.webp)',
+},
+{
+  id: 'card03',
+  title: 'News Blog 3',
+  subtitle: 'HTML, SCSS, JS',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  src: '(./images/travel.webp)',
+}];
+
+projectCard.forEach((element) => {
+  template.querySelector('.newsBlog h3').textContent = element.title;
+  template.querySelector('.newsBlog p').textContent = element.subtitle;
+  template.querySelector('.description').textContent = element.description;
+
+  const clone = document.importNode(template, true);
+  fragment.appendChild(clone);
 });
 
-openModalBtn2.addEventListener('click', () => {
-  modal.showModal();
-});
-
-closeModalBtn.addEventListener('click', () => {
-  modal.close();
-});
+works.appendChild(fragment);
