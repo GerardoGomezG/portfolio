9e177a3f83a1564134b907d9c71dc9733c02e4ed
GerardoGomezG <ing.gerardogomez@gmail.com>
2023-05-09 14:12:52 -0400

    Fixes opening and closing modal windows

--- index.js
+++ index.js
@@ -10,44 +10,32 @@ const template = document.querySelector('template').content;
 const fragment = document.createDocumentFragment();
 
 const projectCard = [{
-  id: 'card01',
+  id: '1',
   title: 'News Blog 1',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
-  alt: 'project snapshot',
 },
 {
-  id: 'card02',
+  id: '2',
   title: 'News Blog 2',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
-  alt: 'project snapshot',
 },
 {
-  id: 'card03',
+  id: '3',
   title: 'News Blog 3',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
-  alt: 'project snapshot',
 },
 {
-  id: 'card03',
-  title: 'News Blog 4',
+  id: '3',
+  title: 'News Blog 3',
   subtitle: 'HTML, SCSS, JS',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   src: './images/travel.webp',
-  alt: 'project snapshot',
-},
-{
-  id: 'card03',
-  title: 'News Blog 5',
-  subtitle: 'HTML, SCSS, JS',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  src: './images/travel.webp',
-  alt: 'project snapshot',
 }];
 
 projectCard.forEach((element) => {
@@ -55,9 +43,12 @@ projectCard.forEach((element) => {
   template.querySelector('.newsBlog h3').textContent = element.title;
   template.querySelector('.newsBlog p').textContent = element.subtitle;
   template.querySelector('.description').textContent = element.description;
+  template.querySelector('.newsBlog button').setAttribute('id', element.id);
+  template.querySelector('.modal').setAttribute('id', `modal${element.id}`);
+  template.querySelector('.closeModal').setAttribute('id', element.id);
   template.querySelector('#projectImg').setAttribute('src', element.src);
-  template.querySelector('#modal h3').textContent = element.title;
-  template.querySelector('#modal p').textContent = element.subtitle;
+  template.querySelector('.modal h3').textContent = element.title;
+  template.querySelector('.modal p').textContent = element.subtitle;
   template.querySelector('#description').textContent = element.description;
 
   const clone = document.importNode(template, true);
@@ -67,23 +58,29 @@ projectCard.forEach((element) => {
 works.appendChild(fragment);
 
 // Popup window
-const openModalBtn = document.querySelector('#openModal');
-const closeModalBtn = document.querySelector('#closeModal');
-const modal = document.querySelector('#modal');
+const openModalBtn = document.querySelectorAll('.openModal');
+const closeModalBtn = document.querySelectorAll('.closeModal');
+console.log(closeModalBtn);
 
-openModalBtn.addEventListener('click', () => {
-  modal.showModal();
+openModalBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    document.getElementById(`modal${e.target.id}`).showModal();
+  });
 });
 
-closeModalBtn.addEventListener('click', () => {
-  modal.close();
+closeModalBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    document.getElementById(`modal${e.currentTarget.id}`).close();
+  });
 });
 
-const y = document.querySelector('.newsBlog');
-y.addEventListener('mouseenter', () => {
-  y.classList.toggle('newsBlogHover');
-});
-
-y.addEventListener('mouseleave', () => {
-  y.classList.toggle('newsBlogHover');
+// newsBlog hover
+const y = document.querySelectorAll('.newsBlog');
+y.forEach((z) => {
+  z.addEventListener('mouseenter', () => {
+    z.classList.toggle('newsBlogHover');
+  });
+  z.addEventListener('mouseleave', () => {
+    z.classList.toggle('newsBlogHover');
+  });
 });
